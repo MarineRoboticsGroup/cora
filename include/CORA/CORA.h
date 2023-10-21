dbbd8c79262dfe03607e82ff6f0f276f9c849dd8
Alan Papalia <alanpapalia@gmail.com>
2023-10-21 09:13:49 -0400

    starting to wireframe the CORA library

---
+++ include/CORA/CORA.h
@@ -0,0 +1,15 @@
+/** @file 
+    @brief The main header file for the CORA library.
+
+    This file provides the primary interface to the CORA library. All
+    usage of the library should be done through this file.
+*/
+
+#pragma once
+
+#include <CORA/CORA_types.h>
+
+namespace CORA
+{
+
+}
---
+++ include/CORA/CORA_types.h
@@ -0,0 +1,15 @@
+/**
+ * @file CORA_types.h
+ * @brief A file containing the basic types used by the CORA library.
+*/
+
+#pragma once
+
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+
+namespace CORA{
+
+    typedef Eigen::VectorXd Vector;
+
+}
