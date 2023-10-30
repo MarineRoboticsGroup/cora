b5b9ba1e5f304a4e5cd52120f096922467e2119d
Alan Papalia <alanpapalia@gmail.com>
2023-10-30 18:47:45 -0400

    beginning attempt of cholesky preconditioner - nothing works yet

--- include/CORA/CORA_preconditioners.h
+++ include/CORA/CORA_preconditioners.h
@@ -12,6 +12,10 @@
 #pragma once
 
 #include <CORA/CORA_types.h>
+#include <vector>
+
+#include <Eigen/Sparse>
+#include <SuiteSparseQR.hpp>
 
 #include "Optimization/Riemannian/Concepts.h"
 #include "Preconditioners/LSChol/include/LSChol/LSChol.h"
@@ -36,11 +40,45 @@ constructCoraBlockCholeskyPreconditioner(const SparseMatrix &A,
   // TODO(magoun): implement this function
   throw NotImplementedException();
 
+  // check that the block sizes sum to A.rows() - 1
+  if (block_sizes.sum() != A.rows() - 1) {
+    throw std::invalid_argument(
+        "The block sizes must sum to A.rows() - 1 for the "
+        "CORA block Cholesky preconditioner");
+  }
+
+  // set the cholmod ordering method to COLAMD
+  int ordering_method = CHOLMOD_COLAMD;
+
   /**
    * @brief compute Cholesky decomposition with permutation ordering to minimize
    * fill-in store the Cholesky factor and the permutation return a
    * LinearOperator that applies the block-wise Cholesky factors
    */
+
+  // for each block along the diagonal of A compute the Cholesky decomposition
+  // of the block and store the Cholesky factor and the permutation
+  // ordering. The permutation ordering is used to minimize fill-in during
+  // the Cholesky decomposition. The Cholesky factor and the permutation
+  // ordering are stored in a LinearOperator that applies the block-wise
+  // Cholesky factors. The LinearOperator is returned.
+  int block_start = 0;
+  std::vector<SuiteSparseQR_factorization<Matrix>> block_cholesky_factors;
+  for (int block_idx = 0; block_idx < block_sizes.size(); block_idx++) {
+    int block_size = block_sizes[block_idx];
+
+    // extract the block from A
+    Eigen::SparseMatrix<double> block =
+        A.block(block_start, block_start, block_sizes[block_idx],
+                block_sizes[block_idx]);
+    SuiteSparseQR_factorization<Eigen::SparseMatrix<double>>
+        block_cholesky_factor =
+            SuiteSparseQR_factorization<Eigen::SparseMatrix<double>>(
+                block, ordering_method);
+
+    // store the block Cholesky factor and the permutation ordering
+    block_cholesky_factors.push_back(block_cholesky_factor);
+  }
 }
 
 /**
