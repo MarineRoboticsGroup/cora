c35fe022ec70dabd675291c96216cdf641d04dc0
Alan Papalia <alanpapalia@gmail.com>
2023-10-31 15:00:01 -0400

    add cholesky and block-cholesky formulations

--- include/CORA/CORA_preconditioners.h
+++ include/CORA/CORA_preconditioners.h
@@ -11,16 +11,19 @@
 
 #pragma once
 
+#include <Eigen/CholmodSupport>
+
 #include <CORA/CORA_types.h>
+#include <memory>
 #include <vector>
 
-#include <Eigen/Sparse>
-#include <SuiteSparseQR.hpp>
-
 #include "Optimization/Riemannian/Concepts.h"
-#include "Preconditioners/LSChol/include/LSChol/LSChol.h"
 
 namespace CORA {
+
+using CholeskyFactorization = Eigen::CholmodDecomposition<SparseMatrix>;
+using CholFactorPtr = std::shared_ptr<CholeskyFactorization>;
+using CholFactorPtrVector = std::vector<CholFactorPtr>;
 
 /**
  * @brief Generate a block-diagonal preconditioner for the given matrix A based
@@ -34,72 +37,50 @@ namespace CORA {
  * @param block_sizes the sizes of the blocks (should sum to A.rows() - 1)
  * @return Optimization::Riemannian::LinearOperator<Matrix, Matrix>
  */
-Optimization::Riemannian::LinearOperator<Matrix, Matrix>
-constructCoraBlockCholeskyPreconditioner(const SparseMatrix &A,
-                                         const Vector &block_sizes) {
-  // TODO(magoun): implement this function
-  throw NotImplementedException();
-
-  // check that the block sizes sum to A.rows() - 1
-  if (block_sizes.sum() != A.rows() - 1) {
-    throw std::invalid_argument(
-        "The block sizes must sum to A.rows() - 1 for the "
-        "CORA block Cholesky preconditioner");
+CholFactorPtrVector getBlockCholeskyFactorization(const SparseMatrix &A,
+                                                  const Vector &block_sizes) {
+  if (block_sizes.sum() != A.rows()) {
+    throw std::invalid_argument("The block sizes must sum to A.rows() for the "
+                                "CORA block Cholesky preconditioner");
   }
-
-  // set the cholmod ordering method to COLAMD
-  int ordering_method = CHOLMOD_COLAMD;
-
-  /**
-   * @brief compute Cholesky decomposition with permutation ordering to minimize
-   * fill-in store the Cholesky factor and the permutation return a
-   * LinearOperator that applies the block-wise Cholesky factors
-   */
 
   // for each block along the diagonal of A compute the Cholesky decomposition
   // of the block and store the Cholesky factor and the permutation
-  // ordering. The permutation ordering is used to minimize fill-in during
-  // the Cholesky decomposition. The Cholesky factor and the permutation
-  // ordering are stored in a LinearOperator that applies the block-wise
-  // Cholesky factors. The LinearOperator is returned.
+  // ordering. The default AMD ordering is used to minimize fill-in during
+  // the Cholesky decomposition.
   int block_start = 0;
-  std::vector<SuiteSparseQR_factorization<Matrix>> block_cholesky_factors;
+  CholFactorPtrVector block_cholesky_factors;
   for (int block_idx = 0; block_idx < block_sizes.size(); block_idx++) {
     int block_size = block_sizes[block_idx];
 
     // extract the block from A
-    Eigen::SparseMatrix<double> block =
+    SparseMatrix block =
         A.block(block_start, block_start, block_sizes[block_idx],
                 block_sizes[block_idx]);
-    SuiteSparseQR_factorization<Eigen::SparseMatrix<double>>
-        block_cholesky_factor =
-            SuiteSparseQR_factorization<Eigen::SparseMatrix<double>>(
-                block, ordering_method);
 
-    // store the block Cholesky factor and the permutation ordering
-    block_cholesky_factors.push_back(block_cholesky_factor);
+    // compute the Cholesky decomposition of the block
+    block_cholesky_factors.emplace_back(
+        std::make_shared<CholeskyFactorization>(block));
   }
+
+  return block_cholesky_factors;
 }
 
-/**
- * @brief Similar to constructCoraBlockCholeskyPreconditioner, but uses the
- * marginalized data matrix, so does not ignore the last row/column of A
- *
- * @param A the matrix to precondition (should be symmetric positive definite)
- * @param block_sizes the sizes of the blocks (should sum to A.rows())
- * @return Optimization::Riemannian::LinearOperator<Matrix, Matrix>
- */
-Optimization::Riemannian::LinearOperator<Matrix, Matrix>
-constructMarginalizedCoraBlockCholeskyPreconditioner(
-    const SparseMatrix &A, const Vector &block_sizes) {
-  // TODO(magoun): implement this function
-  throw NotImplementedException();
+Matrix blockCholeskySolve(CholFactorPtrVector block_chol_factor_ptrs,
+                          int chol_size, const Matrix &rhs) {
+  Matrix result(chol_size, rhs.cols());
+  int block_start = 0;
+  for (int block_idx = 0; block_idx < block_chol_factor_ptrs.size();
+       block_idx++) {
+    int block_size = block_chol_factor_ptrs[block_idx]->rows();
 
-  /**
-   * @brief compute Cholesky decomposition with permutation ordering to minimize
-   * fill-in store the Cholesky factor and the permutation return a
-   * LinearOperator that applies the block-wise Cholesky factors
-   */
+    // print out the block Cholesky factor
+    result.block(block_start, 0, block_size, rhs.cols()) =
+        block_chol_factor_ptrs[block_idx]->solve(
+            rhs.block(block_start, 0, block_size, rhs.cols()));
+    block_start += block_size;
+  }
+  return result;
 }
 
 } // namespace CORA
