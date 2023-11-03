43824ea205ebbc42f17d4bc285c80bc276beeadf
Alan Papalia <alanpapalia@gmail.com>
2023-11-02 22:20:53 -0400

    set up some basic tests to check that all matrices are correctly being formed

--- tests/test_construct_problem.cpp
+++ tests/test_construct_problem.cpp
@@ -10,13 +10,18 @@
  */
 
 #include <CORA/CORA_problem.h>
+#include <CORA/CORA_utils.h>
+
+#include <filesystem>
+#include <string>
+
 #include <catch2/catch_test_macros.hpp>
 
 namespace CORA {
 
 TEST_CASE("constructing single-odometry data matrix",
           "[problem::data_matrix::single-odom]") {
-  int dim = 3;
+  int dim = 2;
   int rank = 5;
   Problem problem(dim, rank, Formulation::Explicit);
   Symbol x1("x1");
@@ -24,39 +29,51 @@ TEST_CASE("constructing single-odometry data matrix",
   problem.addPoseVariable(x1);
   problem.addPoseVariable(x2);
 
-  // test when odometry measurement is identity (no rotation, no translation)
-  Matrix rotId = Matrix::Identity(3, 3);
-  Vector tranZero = Vector::Zero(3);
-  Matrix covId = Matrix::Identity(6, 6);
-  RelativePoseMeasurement rpm_identity(x1, x2, rotId, tranZero, covId);
+  // test when odometry measurement is moving in the x direction by 1
+  Matrix rotId = Matrix::Identity(2, 2);
+  Vector tran = Vector::Zero(2);
+  tran(0) = 1.0;
+  Matrix covId = Matrix::Identity(3, 3);
+  RelativePoseMeasurement rpm_identity(x1, x2, rotId, tran, covId);
   problem.addRelativePoseMeasurement(rpm_identity);
-  problem.constructDataMatrix();
-  SparseMatrix data_matrix = problem.getDataMatrix();
 
-  Matrix expected_state_vals = Matrix::Zero((dim + 1) * 2, dim);
-  expected_state_vals.block(0, 0, dim, dim) = rotId;
-  expected_state_vals.block(dim, 0, dim, dim) = rotId;
-  Matrix expected_translations = Matrix::Zero(2, dim);
-  expected_translations.row(0) = Vector::Random(dim);
-  expected_translations.row(1) = expected_translations.row(0);
+  SECTION("Matrices are correct") {
+    std::string err_msg = checkSubmatricesAreCorrect(problem, "single_rpm");
+    REQUIRE(err_msg == "");
+  }
 
-  expected_state_vals.block(2 * dim, 0, 2, dim) = expected_translations;
+  SECTION("GT values are in null space") {
+    SparseMatrix data_matrix = problem.getDataMatrix();
 
-  // the expected state values should be in null space of the data matrix
-  Matrix mat_prod = data_matrix * expected_state_vals;
-  REQUIRE(mat_prod.norm() < 1e-12);
+    // check if data matrix is initialized
+    REQUIRE(data_matrix.rows() > 0);
+    REQUIRE(data_matrix.cols() > 0);
 
-  // we should be able to multiply the state values by any (d x d) orthogonal
-  // matrix and still be in the null space of the data matrix
+    // construct the ground truth state values
+    Matrix expected_state_vals = Matrix::Zero((dim + 1) * 2, dim);
+    expected_state_vals.block(0, 0, dim, dim) = rotId;
+    expected_state_vals.block(dim, 0, dim, dim) = rotId;
+    Matrix expected_translations = Matrix::Zero(2, dim);
+    expected_translations.row(0) = Vector::Random(dim);
+    expected_translations.row(1) =
+        expected_translations.row(0) + tran.transpose();
+    expected_state_vals.block(2 * dim, 0, 2, dim) = expected_translations;
 
-  // we can get a random orthogonal matrix by taking the left singular vectors
-  // of a random matrix
-  Matrix A(dim, dim);
-  Eigen::JacobiSVD<Matrix> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
-  Matrix random_ortho = svd.matrixU();
+    // the expected state values should be in null space of the data matrix
+    Matrix mat_prod = data_matrix * expected_state_vals;
+    REQUIRE(mat_prod.norm() < 1e-12);
 
-  mat_prod = data_matrix * (expected_state_vals * random_ortho);
-  REQUIRE(mat_prod.norm() < 1e-12);
+    // we should be able to multiply the state values by any (d x d) orthogonal
+    // matrix and still be in the null space of the data matrix.  we can get a
+    // random orthogonal matrix by taking the left singular vectors of a random
+    // matrix
+    Matrix A(dim, dim);
+    Eigen::JacobiSVD<Matrix> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Matrix random_ortho = svd.matrixU();
+
+    mat_prod = data_matrix * (expected_state_vals * random_ortho);
+    REQUIRE(mat_prod.norm() < 1e-12);
+  }
 }
 
 TEST_CASE("constructing single-range data matrix",
@@ -69,11 +86,18 @@ TEST_CASE("constructing single-range data matrix",
   problem.addLandmarkVariable(l1);
   problem.addLandmarkVariable(l2);
   Scalar range_dist = 2.0;
-  RangeMeasurement range_measure = RangeMeasurement(l1, l2, range_dist, 0.5);
+  RangeMeasurement range_measure = RangeMeasurement(l1, l2, range_dist, 1.0);
   problem.addRangeMeasurement(range_measure);
 
-  problem.constructDataMatrix();
-  SparseMatrix data_matrix = problem.getDataMatrix();
+  /**
+   * @brief Series of tests to make sure that the submatrices of the data matrix
+   * match the expected values
+   *
+   */
+  SECTION("Matrices are correct") {
+    std::string err_msg = checkSubmatricesAreCorrect(problem, "single_range");
+    REQUIRE(err_msg == "");
+  }
 
   /**
    * @brief We will construct an expected state matrix with l1 and l2 range_dist
@@ -83,18 +107,21 @@ TEST_CASE("constructing single-range data matrix",
    * The expected state matrix ordering: [offset direction; l1; l2]
    *
    */
-  Vector l1_pos = Vector::Random(dim);
-  Vector l2_offset_dir = Vector::Random(dim);
-  l2_offset_dir.normalize();
-  Vector l2_pos = l1_pos + l2_offset_dir * range_dist;
+  SECTION("True values in null space") {
+    Vector l1_pos = Vector::Random(dim);
+    Vector l2_offset_dir = Vector::Random(dim);
+    l2_offset_dir.normalize();
+    Vector l2_pos = l1_pos + l2_offset_dir * range_dist;
 
-  Matrix expected_state_vals = Matrix::Zero(3, dim);
-  expected_state_vals.row(0) = l2_offset_dir;
-  expected_state_vals.row(1) = l1_pos;
-  expected_state_vals.row(2) = l2_pos;
+    Matrix expected_state_vals = Matrix::Zero(3, dim);
+    expected_state_vals.row(0) = -l2_offset_dir;
+    expected_state_vals.row(1) = l1_pos;
+    expected_state_vals.row(2) = l2_pos;
 
-  // the expected state values should be in null space of the data matrix
-  Matrix mat_prod = data_matrix * expected_state_vals;
-  REQUIRE(mat_prod.norm() < 1e-12);
+    // the expected state values should be in null space of the data matrix
+    SparseMatrix data_matrix = problem.getDataMatrix();
+    Matrix mat_prod = data_matrix * expected_state_vals;
+    REQUIRE(mat_prod.norm() < 1e-12);
+  }
 }
 } // namespace CORA
--- tests/test_parse_pyfg.cpp
+++ tests/test_parse_pyfg.cpp
@@ -2,13 +2,31 @@
 // Created by Tim Magoun on 10/31/23.
 //
 
+#include <CORA/CORA_utils.h>
 #include <CORA/pyfg_text_parser.h>
+
+#include <filesystem>
+#include <string>
+
 #include <catch2/catch_test_macros.hpp>
 
+namespace CORA {
 TEST_CASE("Loading PyFg", "[Read PyFg]") {
-  // TODO(magoun): Figure out how to test this without knowing example file
-  // location
-  //  auto filename = "/tmp/cora-plus-plus/examples/factor_graph.pyfg";
-  //  auto problem = CORA::parsePyfgTextToProblem(filename);
-  REQUIRE(true);
+  std::string small_sample_subdir = "small_ra_slam_problem";
+  std::string pyfg_path =
+      getTestDataFpath(small_sample_subdir, "factor_graph.pyfg");
+
+  Problem problem = parsePyfgTextToProblem(pyfg_path);
+  std::string err_msg =
+      checkSubmatricesAreCorrect(problem, small_sample_subdir);
+  REQUIRE(err_msg == "");
+
+  // // DEBUGGING
+  // SparseMatrix mat_diff = data_matrix - expected_matrix;
+  // mat_diff.prune(0.5, 1.0);
+  // std::cout << "data_matrix - expected_matrix = \n"
+  //           << mat_diff.toDense() << std::endl;
+  // printMatrixSparsityPattern(mat_diff.toDense());
+  // // DEBUGGING
 }
+} // namespace CORA
