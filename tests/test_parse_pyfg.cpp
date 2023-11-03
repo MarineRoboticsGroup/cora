dcda503a1235a94f37d26d20d618b2f1f3ec5f2a
Alan Papalia <alanpapalia@gmail.com>
2023-11-02 22:47:47 -0400

    added more tests for PyFG parsing

--- tests/test_parse_pyfg.cpp
+++ tests/test_parse_pyfg.cpp
@@ -11,22 +11,28 @@
 #include <catch2/catch_test_macros.hpp>
 
 namespace CORA {
-TEST_CASE("Loading PyFg", "[Read PyFg]") {
-  std::string small_sample_subdir = "small_ra_slam_problem";
-  std::string pyfg_path =
-      getTestDataFpath(small_sample_subdir, "factor_graph.pyfg");
-
+TEST_CASE("Loading PyFg", "[ParsePyFG::small_ra_slam_problem]") {
+  std::string data_subdir = "small_ra_slam_problem";
+  std::string pyfg_path = getTestDataFpath(data_subdir, "factor_graph.pyfg");
   Problem problem = parsePyfgTextToProblem(pyfg_path);
-  std::string err_msg =
-      checkSubmatricesAreCorrect(problem, small_sample_subdir);
+  std::string err_msg = checkSubmatricesAreCorrect(problem, data_subdir);
   REQUIRE(err_msg == "");
-
-  // // DEBUGGING
-  // SparseMatrix mat_diff = data_matrix - expected_matrix;
-  // mat_diff.prune(0.5, 1.0);
-  // std::cout << "data_matrix - expected_matrix = \n"
-  //           << mat_diff.toDense() << std::endl;
-  // printMatrixSparsityPattern(mat_diff.toDense());
-  // // DEBUGGING
 }
+
+TEST_CASE("Loading PyFg", "[ParsePyFG::single_range]") {
+  std::string data_subdir = "single_range";
+  std::string pyfg_path = getTestDataFpath(data_subdir, "factor_graph.pyfg");
+  Problem problem = parsePyfgTextToProblem(pyfg_path);
+  std::string err_msg = checkSubmatricesAreCorrect(problem, data_subdir);
+  REQUIRE(err_msg == "");
+}
+
+TEST_CASE("Loading PyFg", "[ParsePyFG::single_rpm]") {
+  std::string data_subdir = "single_rpm";
+  std::string pyfg_path = getTestDataFpath(data_subdir, "factor_graph.pyfg");
+  Problem problem = parsePyfgTextToProblem(pyfg_path);
+  std::string err_msg = checkSubmatricesAreCorrect(problem, data_subdir);
+  REQUIRE(err_msg == "");
+}
+
 } // namespace CORA
