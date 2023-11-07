c582a7b1c385df7f114dc5fb41e328d162e87f3c
Alan Papalia <alanpapalia@gmail.com>
2023-11-07 12:56:56 -0500

    sketch out some of the necessary functions for optimization

--- src/CORA.cpp
+++ src/CORA.cpp
@@ -6,19 +6,58 @@ namespace CORA {
 
 using CoraTntResult = Optimization::Riemannian::TNTResult<Matrix, Scalar>;
 
-Matrix solveCORA(const Problem &problem) {
-  Optimization::Objective<Matrix, Scalar, Matrix> f;
-  Optimization::Riemannian::QuadraticModel<Matrix, Matrix, Matrix> QM;
-  Optimization::Riemannian::RiemannianMetric<Matrix, Matrix, Scalar, Matrix>
-      metric;
-  Optimization::Riemannian::Retraction<Matrix, Matrix, Matrix> retract;
-  Matrix x0;
+Matrix solveCORA(const Problem &problem, const Matrix &x0) {
+  // objective function
+  Optimization::Objective<Matrix, Scalar, Matrix> f =
+      [&problem](const Matrix &Y, const Matrix &NablaF_Y) {
+        return problem.evaluate_objective(Y);
+      };
+
+  // quadratic model
+  Optimization::Riemannian::QuadraticModel<Matrix, Matrix, Matrix> QM =
+      [&problem](const Matrix &Y, Matrix &grad,
+                 Optimization::Riemannian::LinearOperator<Matrix, Matrix,
+                                                          Matrix> &HessOp,
+                 Matrix &NablaF_Y) {
+        // Compute and cache Euclidean gradient at the current iterate
+        NablaF_Y = problem.Euclidean_gradient(Y);
+
+        // Compute Riemannian gradient from Euclidean gradient
+        grad = problem.Riemannian_gradient(Y, NablaF_Y);
+
+        // Define linear operator for computing Riemannian Hessian-vector
+        // products (cf. eq. (44) in the SE-Sync tech report)
+        HessOp = [&problem](const Matrix &Y, const Matrix &Ydot,
+                            const Matrix &NablaF_Y) {
+          return problem.Riemannian_Hessian_vector_product(Y, NablaF_Y, Ydot);
+        };
+      };
+
+  // get retraction from problem
+  Optimization::Riemannian::Retraction<Matrix, Matrix, Matrix> retract =
+      [&problem](const Matrix &Y, const Matrix &V, const Matrix &NablaF_Y) {
+        return problem.Riemannian_retraction(Y, V);
+      };
+
+  // Euclidean gradient (not used)
   Matrix NablaF_Y;
+
+  // get preconditioner from problem
   std::optional<
       Optimization::Riemannian::LinearOperator<Matrix, Matrix, Matrix>>
       precon;
+
+  // default TNT parameters
   Optimization::Riemannian::TNTParams<Scalar> params;
-  std::optional<InstrumentationFunction> user_function;
+
+  // metric over the tangent space is the standard matrix trace inner product
+  Optimization::Riemannian::RiemannianMetric<Matrix, Matrix, Scalar, Matrix>
+      metric =
+          [](const Matrix &Y, const Matrix &V1, const Matrix &V2,
+             const Matrix &NablaF_Y) { return (V1.transpose() * V2).trace(); };
+
+  // no custom instrumentation function for now
+  std::optional<InstrumentationFunction> user_function = std::nullopt;
 
   CoraTntResult result =
       Optimization::Riemannian::TNT<Matrix, Matrix, Scalar, Matrix>(
