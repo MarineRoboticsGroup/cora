b10abac65152f21d9fc9cbdd964981319c59b796
Alan Papalia <alanpapalia@gmail.com>
2023-11-08 10:41:28 -0500

    finish off functionality for oblique manifold

--- include/CORA/ObliqueManifold.h
+++ include/CORA/ObliqueManifold.h
@@ -39,12 +39,24 @@ public:
   /// GEOMETRY
 
   /** Given a generic matrix A in R^{r x n}, this function projects A onto the
-   * oblique manifold by normalizing each column of A to have unit norm. */
+   * oblique manifold by normalizing each column of A to have unit norm. Note
+   * that this is a computationally cheap operation but is not a second-order
+   * retraction. See Boumal "Optimization on Smooth Manifolds" for more details.
+   */
   Matrix projectToManifold(const Matrix &A) const;
 
   /**
-   * @brief Projects a matrix A in R^{r x n} onto the tangent space of the
-   * oblique manifold at Y in R^{r x n}.
+   * @brief Compute the Frobenius inner product of A and B in R^{r x n}.
+   *
+   * @param A the first matrix
+   * @param B the second matrix
+   * @return Scalar the inner product of A and B
+   */
+  Scalar innerProduct(const Matrix &A, const Matrix &B) const;
+
+  /**
+   * @brief Projects a matrix A in R^{r x n} onto the tangent space T_Y(M) of
+   * the oblique manifold at Y in R^{r x n}.
    *
    * @param A the matrix to project
    * @param Y the point defining the tangent space
@@ -53,9 +65,8 @@ public:
   Matrix projectToTangentSpace(const Matrix &A, const Matrix &Y) const;
 
   /** Given an element Y in M and a tangent vector V in T_Y(M), this function
-   * computes the retraction along V at Y using the QR-based retraction
-   * specified in eq. (4.8) of Absil et al.'s  "Optimization Algorithms on
-   * Matrix Manifolds").
+   * computes the retraction along V at Y using the projectToManifold method
+   *
    */
   Matrix retract(const Matrix &Y, const Matrix &V) const;
 
--- src/ObliqueManifold.cpp
+++ src/ObliqueManifold.cpp
@@ -12,4 +12,42 @@ Matrix ObliqueManifold::projectToManifold(const Matrix &A) const {
   return A_normalized;
 }
 
+Matrix ObliqueManifold::projectToTangentSpace(const Matrix &A,
+                                              const Matrix &Y) const {
+  // get the inner product by taking the row-wise summation of the Hadamard
+  // product of Y and A
+  Vector inner_prods = (Y.array() * A.array()).colwise().sum();
+
+  // we now want to scale each column of Y by the corresponding inner product
+  // and subtract the result from A
+  Matrix scaled_cols = Y.array().rowwise() * inner_prods.transpose().array();
+  return A - scaled_cols;
+}
+
+Matrix ObliqueManifold::retract(const Matrix &Y, const Matrix &V) const {
+  return projectToManifold(Y + V);
+}
+
+Matrix ObliqueManifold::random_sample(
+    const std::default_random_engine::result_type &seed) const {
+  // initialize the random number generator
+  std::default_random_engine generator(seed);
+
+  // sample each column of the matrix from the standard normal distribution
+  std::normal_distribution<Scalar> g;
+  Matrix A(r_, n_);
+  for (int64_t j = 0; j < A.cols(); j++) {
+    for (int64_t i = 0; i < A.rows(); i++) {
+      A(i, j) = g(generator);
+    }
+  }
+  return projectToManifold(A);
+}
+
+Scalar ObliqueManifold::innerProduct(const Matrix &A, const Matrix &B) const {
+  // get the inner product by taking the row-wise summation of the Hadamard
+  // product of A and B
+  return (A.array() * B.array()).sum();
+}
+
 } // namespace CORA
