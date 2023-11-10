ceca565dae5ce54ac382555f02e9945329a6d98e
Alan Papalia <alanpapalia@gmail.com>
2023-11-10 09:01:25 -0500

    add a few more small tests to guide debugging

--- tests/test_cora.cpp
+++ tests/test_cora.cpp
@@ -9,19 +9,15 @@
 
 namespace CORA {
 
-void printResults(CoraTntResult res, Problem problem, Matrix X_gt,
+void printResults(CoraTntResult res, Problem problem, Matrix x0,
                   std::string data_subdir) {
   std::cout << "_____________________________________" << std::endl;
   std::cout << "Data subdir: " << data_subdir << std::endl;
-  std::cout << "Cost at GT: " << problem.evaluateObjective(X_gt) << std::endl;
-  std::cout << "Egrad norm at GT: " << problem.Euclidean_gradient(X_gt).norm()
+  std::cout << "Cost at init: " << problem.evaluateObjective(x0) << std::endl;
+  std::cout << "Egrad norm at init: " << problem.Euclidean_gradient(x0).norm()
             << std::endl;
-  Matrix Rgrad = problem.Riemannian_gradient(X_gt);
-  std::cout << "Rgrad norm at GT: " << Rgrad.norm() << std::endl;
-  if (Rgrad.norm() > 1e-8) {
-    std::cout << "Rgrad at GT: " << std::endl << Rgrad << std::endl;
-  }
-
+  Matrix Rgrad = problem.Riemannian_gradient(x0);
+  std::cout << "Rgrad norm at init: " << Rgrad.norm() << std::endl;
   std::cout << "Run time: " << res.elapsed_time << std::endl;
   std::cout << "Estimated state: " << std::endl << res.x << std::endl;
   std::cout << "Cost: " << res.f << std::endl;
@@ -32,6 +28,7 @@ void printResults(CoraTntResult res, Problem problem, Matrix X_gt,
   for (Scalar val : res.preconditioned_gradient_norms) {
     std::cout << val << "; ";
   }
+  std::cout << "\n_____________________________________" << std::endl;
   std::cout << "\n\n\n" << std::endl;
 }
 
@@ -50,7 +47,13 @@ void testScenario(std::string data_subdir) {
 
   // just check if it runs
   CoraTntResult res = solveCORA(problem, X_gt);
+  std::cout << "Testing with Ground Truth initialization" << std::endl;
   printResults(res, problem, X_gt, data_subdir);
+
+  // just check if it runs
+  res = solveCORA(problem, x0);
+  std::cout << "Testing with Random initialization" << std::endl;
+  printResults(res, problem, x0, data_subdir);
 }
 
 TEST_CASE("Test solve RA-SLAM", "[CORA-solve::small_ra_slam_problem]") {
