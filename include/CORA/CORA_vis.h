0475f158402ffde8caf3ab176d77320f7c1afccf
Tim Magoun <timmagoun@gmail.com>
2023-11-12 16:29:43 -0500

    Added blank files

---
+++ include/CORA/CORA_vis.h
@@ -0,0 +1,10 @@
+//
+// Created by tim on 11/12/23.
+//
+
+#ifndef CORA_CORA_VIS_H
+#define CORA_CORA_VIS_H
+
+class CORA_vis {};
+
+#endif // CORA_CORA_VIS_H
---
+++ src/CORA_vis.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tim on 11/12/23.
+//
+
+#include "CORA/CORA_vis.h"
