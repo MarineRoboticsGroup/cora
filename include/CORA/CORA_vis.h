ee75e034204422a7ceee33e79356140d3cfb88dd
Alan Papalia <alanpapalia@gmail.com>
2023-11-14 20:04:25 -0500

    header guard -> pragma once

--- include/CORA/CORA_vis.h
+++ include/CORA/CORA_vis.h
@@ -2,8 +2,7 @@
 // Created by tim on 11/12/23.
 //
 
-#ifndef CORA_CORA_VIS_H
-#define CORA_CORA_VIS_H
+#pragma once
 
 #include <CORA/CORA.h>
 #include <CORA/CORA_types.h>
@@ -30,5 +29,3 @@ private:
 };
 
 } // namespace CORA
-
-#endif // CORA_CORA_VIS_H
