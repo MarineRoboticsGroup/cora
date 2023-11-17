7a66189589d256f6e9fc692b24f5eaa2c413fa04
Tim Magoun <timmagoun@gmail.com>
2023-11-17 16:02:28 -0500

    fixed struct

--- src/CORA_vis.cpp
+++ src/CORA_vis.cpp
@@ -27,8 +27,8 @@ void CORAVis::run(const Problem &problem, std::vector<CoraTntResult> results,
   params.landtype = mrg::LandmarkDrawType::kCross;
   params.f = 600.0f; // focal length in px
   params.rangetype = mrg::RangeDrawType::kLine;
-  params.range_color = std::make_tuple(0.0f, 0.71f, 0.16f);  // Medium green
-  params.landmark_color = std::make_tuple(0.0f, 0.0f, 0.0f); // Black
+  params.range_color = {0.0f, 0.71f, 0.16f};  // Medium green
+  params.landmark_color = {0.0f, 0.0f, 0.0f}; // Black
 
   auto viz = std::make_shared<mrg::Visualizer>(params);
   auto render_thread = std::thread(&CORAVis::renderLoop, this, viz);
